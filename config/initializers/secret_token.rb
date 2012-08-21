dcf116e47fc708c09e6b9d15844091c98a3eb0de
bbqnow <bbqfast@hotmail.com>
2012-08-20 18:26:35 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'c6d769318bcb2e8c3f4ddacda2dcc8919c2381d60cb602c1ce3b66401a60af842013ec958c7952a6f003ca2dad3b9b5b519bef6bf5d3d5e84aa0721bbbd3871f'
